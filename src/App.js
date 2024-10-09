0eb5553c68d368e076e01d96c65cde53ed7744c8
Idris K <kubomuedwin@gmail.com>
2024-10-09 12:21:04 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+import Services from './components/Services';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <Services />
+      <Portfolio />
+      <Contact />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
