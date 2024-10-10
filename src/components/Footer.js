287b6a6b2f5a42108394d447a7acaad39e337d93
Idris K <kubomuedwin@gmail.com>
2024-10-10 08:16:41 +0300

    Social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,11 +25,9 @@ const Footer = () => {
             <h3>Follow Us</h3>
             <a href="https://x.com/KubomuEdi" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faXTwitter} className="icon" />
-              X
             </a>
             <a href="https://github.com/Kubomu" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faGithub} className="icon" />
-              GitHub
             </a>
           </div>
         </div>
